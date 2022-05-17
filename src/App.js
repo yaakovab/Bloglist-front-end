6328b59e0e8bf31e5df9ab32d4902877eefd1341
yaakovab <jia17511@gmail.com>
2022-05-17 20:56:38 +0300

    finished 5.1-5.4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './index.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,14 +21,52 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const userJson = window.localStorage.getItem('loggedUser')
+    if (userJson) {
+      const user = JSON.parse(userJson)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
     console.log('logging in', username, password)
 
-    const user = await loginService.login({ username, password })
-    setUser(user)
-    setUsername('')
-    setPassword('')
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+      setUser(user)
+      blogService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setMessage('wrong username or password')
+      setTimeout(() => setMessage(null), 5000)
+    }
+  }
+
+  const handleLogout = async event => {
+    event.preventDefault()
+    console.log('logging out')
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
+  const addBlog = async event => {
+    event.preventDefault()
+    const newBlog = {
+      title, author, url
+    }
+    const response = await blogService.create(newBlog)
+    setBlogs(blogs.concat(response))
+    setMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+    setTimeout(() => setMessage(null), 5000)
+    setAuthor('')
+    setTitle('')
+    setUrl('')
+    console.log('new blog added', title, author, url)
   }
 
   const loginForm = () =>
@@ -30,6 +74,7 @@ const App = () => {
       <h2>
         log in to application
       </h2>
+      <Notification message={message} />
       <div>
         username
         <input
@@ -57,6 +102,33 @@ const App = () => {
       {user === null ? loginForm() :
         <div>
           <h2>blogs</h2>
+          <Notification message={message} />
+          <p>{user.username} logged in<button onClick={handleLogout} >logout</button></p>
+          <form onSubmit={addBlog}>
+            <h2>create new</h2>
+            <div>
+              title: <input
+                type="text"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
+              />
+            </div>
+            <div>
+              author: <input
+                type="text"
+                value={author}
+                onChange={({ target }) => setAuthor(target.value)}
+              />
+            </div>
+            <div>
+              url: <input
+                type="url"
+                value={url}
+                onChange={({ target }) => setUrl(target.value)}
+              />
+            </div>
+            <button onClick={addBlog}>create</button>
+          </form>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -6,4 +6,20 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const create = async newBlog => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newBlog, config)
+  return response.data
+}
+
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+export default { getAll, create, setToken }
