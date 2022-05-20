51cab231911a01db18f699aad3d17eb67bdae651
yaakovab <jia17511@gmail.com>
2022-05-20 11:47:34 +0300

    refactored new blog form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
+import NewBlogForm from './components/NewBlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import './index.css'
@@ -10,9 +11,7 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
 
   const [user, setUser] = useState(null)
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+
   const [message, setMessage] = useState(null)
 
   useEffect(() => {
@@ -44,51 +43,14 @@ const App = () => {
     setUser(null)
   }
 
-  const addBlog = async event => {
-    event.preventDefault()
-    const newBlog = {
-      title, author, url
-    }
-    const response = await blogService.create(newBlog)
+  const addBlog = async blogObject => {
+    const response = await blogService.create(blogObject)
     setBlogs(blogs.concat(response))
-    setMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+    setMessage(`a new blog ${response.title} by ${response.author} added`)
     setTimeout(() => setMessage(null), 5000)
-    setAuthor('')
-    setTitle('')
-    setUrl('')
-    console.log('new blog added', title, author, url)
   }
 
 
-
-  const newBlogForm = () => {
-    return (
-      <form onSubmit={addBlog}>
-        <h2>create new</h2>
-        <div>
-          title: <input
-            type="text"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author: <input
-            type="text"
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url: <input
-            type="url"
-            value={url}
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button onClick={addBlog}>create</button>
-      </form>)
-  }
   return (
     <>
       {user === null ? <LoginForm newLogin={handleLogin} message={message} setMessage={setMessage} /> :
@@ -96,7 +58,7 @@ const App = () => {
           <h2>blogs</h2>
           <Notification message={message} />
           <p>{user.username} logged in<button onClick={handleLogout} >logout</button></p>
-          {newBlogForm()}
+          <NewBlogForm addNewBlog={addBlog} />
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
---
+++ src/components/NewBlogForm.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const NewBlogForm = ({ addNewBlog }) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    const addBlog = async (event) => {
+        event.preventDefault()
+        addNewBlog({ title, author, url })
+        setAuthor('')
+        setTitle('')
+        setUrl('')
+        console.log('new blog added', title, author, url)
+    }
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={addBlog}>
+                <div>
+                    title: <input
+                        type="text"
+                        value={title}
+                        onChange={({ target }) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author: <input
+                        type="text"
+                        value={author}
+                        onChange={({ target }) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    url: <input
+                        type="url"
+                        value={url}
+                        onChange={({ target }) => setUrl(target.value)}
+                    />
+                </div>
+                <button type="submit" >create</button>
+            </form>
+        </div>
+    )
+}
+
+export default NewBlogForm
